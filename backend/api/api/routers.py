2fd9588dc2e8efb853a632ff4c971886ae59c19d
Suzi KC <kcsushmita9742@gmail.com>
2023-09-26 20:16:50 +0100

    Add Dockerfile and health check api for backend app

--- backend/api/api/routers.py
+++ backend/api/api/routers.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from api.api.handlers import products, categories, banners, users, orders
 from api.api.auth.jwt import auth_router
+from api.api.health.health_check import health_check_router
 
 router =  APIRouter()
 
@@ -10,3 +11,4 @@ router.include_router(banners.banner_router, prefix="/banner", tags=["banner"])
 router.include_router(users.user_router, prefix="/user", tags=["user"])
 router.include_router(orders.order_router, prefix="/order", tags=["order"])
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
+router.include_router(health_check_router, prefix="/health",tags=["health"])
