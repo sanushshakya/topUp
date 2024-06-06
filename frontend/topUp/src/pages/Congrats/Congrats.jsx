26c771f7d0b0427bcd6afa779e219c388547ab98
sanush7 <sanush.shakya@paper2product.co.uk>
2024-06-06 20:50:30 +0545

    redesign for category product add

--- frontend/topUp/src/pages/Congrats/Congrats.jsx
+++ frontend/topUp/src/pages/Congrats/Congrats.jsx
@@ -1,28 +1,27 @@
-import React, {useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom';
-import './Congrats.scss'
-import Cookies from 'js-cookie'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./Congrats.scss";
+import Cookies from "js-cookie";
 
 const Congrats = () => {
-  const accessToken = Cookies.get('accessToken');
+  const accessToken = Cookies.get("accessToken");
   const { token } = useParams();
   const [isLoggedIn, setIsLoggedIn] = useState(!!Cookies.get("accessToken"));
-  {!isLoggedIn && (
-    window.location.href = '/login'
-  )}
+  {
+    !isLoggedIn && (window.location.href = "/login");
+  }
   return (
-    <div className='congrats'>
-        <div className="container">
-            <span>
-                Congratulations!
-            </span>
-            <span className='desc'>
-              Your redeemable token: {token}<br/>
-              We've also sent you this token in your email.
-            </span>
-        </div>
+    <div className="congrats">
+      <div className="container">
+        <span>Congratulations!</span>
+        <span className="desc">
+          Your redeemable token: {token}
+          <br />
+          We've also sent you this token in your email.
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Congrats
+export default Congrats;
--- frontend/topUp/src/pages/CreateCat/CreateCat.jsx
+++ frontend/topUp/src/pages/CreateCat/CreateCat.jsx
@@ -1,89 +1,103 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Cookies from 'js-cookie'
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import './CreateCat.scss'
-import config from '../../config'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import "./CreateCat.scss";
+import config from "../../config";
 
 const CreateCat = () => {
-    const accessToken = Cookies.get('accessToken')
-    const [user, setUser] = useState([]);
-    useEffect(() => {
-        if (accessToken) {
-            const fetchData = async () => {
-                try {
-                    const response = await axios.post(`${config.apiBaseUrl}/api/auth/test-token/${accessToken}`)
-                    setUser(response.data)
-                } catch (error) {
-                    console.error(error);
-                }
-            }
-            fetchData();
-        }
-    }, [])
-
-    const schema = yup.object().shape({
-        cat_name: yup.string().required('Category required'),
-        description: yup.string().required('Description is required'),
-        image_url: yup.mixed().required('Profile image is required'),
-    });
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm({
-        resolver: yupResolver(schema),
-    });
-
-    const onSubmit = async (data) => {
-        const formData = new FormData();
-        formData.append('cat_name', data.cat_name);
-        formData.append('description', data.description);
-        formData.append('image_url', data.image_url[0]);
+  const accessToken = Cookies.get("accessToken");
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    if (accessToken) {
+      const fetchData = async () => {
         try {
-            await axios.post(`${config.apiBaseUrl}/api/category/create`, formData, {
-                params: {
-                  token: accessToken
-                }
-              });
-            window.location.href = `/`
-            // Handle the response as needed
+          const response = await axios.post(
+            `${config.apiBaseUrl}/api/auth/test-token/${accessToken}`
+          );
+          setUser(response.data);
         } catch (error) {
-            console.error(error.response?.data || error);
-            // Handle the error
+          console.error(error);
         }
+      };
+      fetchData();
     }
-    return (
-        <div className='cat'>
-            <div className="container">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <h1>Add New Category</h1>
-                    <div className="credentials">
-                        <span>
-                            <input {...register('cat_name')} type="text" placeholder='Enter category name ' />
-                        </span>
-                        {errors.cat_name && (
-                            <span className="error-message">{errors.cat_name.message}</span>
-                        )}
-                        <span>
-                            <input {...register('description')} type="text" placeholder='Enter about this category' />
-                        </span>
-                        {errors.description && (
-                            <span className="error-message">{errors.description.message}</span>
-                        )}
-                        <h4>Upload Profile:</h4>
-                        <input {...register('image_url')} type="file" />
-                        {errors.image_url && (
-                            <span className="error-message">{errors.image_url.message}</span>
-                        )}
-                    </div>
-                    <button type="submit">Add Category</button>
-                </form>
-            </div>
-        </div>
-    )
-}
+  }, []);
 
-export default CreateCat
+  const schema = yup.object().shape({
+    cat_name: yup.string().required("Category required"),
+    description: yup.string().required("Description is required"),
+    image_url: yup.mixed().required("Profile image is required"),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async (data) => {
+    const formData = new FormData();
+    formData.append("cat_name", data.cat_name);
+    formData.append("description", data.description);
+    formData.append("image_url", data.image_url[0]);
+    try {
+      await axios.post(`${config.apiBaseUrl}/api/category/create`, formData, {
+        params: {
+          token: accessToken,
+        },
+      });
+      window.location.href = `/`;
+      // Handle the response as needed
+    } catch (error) {
+      console.error(error.response?.data || error);
+      // Handle the error
+    }
+  };
+  return (
+    <div className="cat">
+      <div className="container">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2>Add New Category</h2>
+          <div className="credentials">
+            <span>
+              <input
+                {...register("cat_name")}
+                type="text"
+                placeholder="Enter category name "
+              />
+            </span>
+            {errors.cat_name && (
+              <span className="error-message">{errors.cat_name.message}</span>
+            )}
+            <span>
+              <input
+                {...register("description")}
+                type="text"
+                placeholder="Enter about this category"
+              />
+            </span>
+            {errors.description && (
+              <span className="error-message">
+                {errors.description.message}
+              </span>
+            )}
+            <h4>Upload Profile:</h4>
+            <input {...register("image_url")} type="file" />
+            {errors.image_url && (
+              <span className="error-message">{errors.image_url.message}</span>
+            )}
+          </div>
+          <button type="submit" className="btn-nav">
+            Add Category
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateCat;
